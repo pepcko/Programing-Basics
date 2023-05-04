c7d309576efc94546d378064bce5ffc8824ac122
Петър Попов <124499412+pepcko@users.noreply.github.com>
2023-05-04 07:15:55 +0300

    Update 04_transport_price.py

--- conditional_statments/conditional_statments_more/04_transport_price.py
+++ conditional_statments/conditional_statments_more/04_transport_price.py
@@ -2,11 +2,10 @@ km_to_travel = int(input())
 time_of_travel = input()
 
 price = 0
-taxi_starting_price = 0
+taxi_starting_price = 0.7
 
 if km_to_travel < 20:
-    
-    taxi_starting_price += 0.7
+
     if time_of_travel == "day":
         price = 0.79
     elif time_of_travel == "night":
