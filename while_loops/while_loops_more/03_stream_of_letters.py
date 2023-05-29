0db8cdd03c62f5ca616ff1f427ac3858dca5742a
Петър Попов <124499412+pepcko@users.noreply.github.com>
2023-05-29 21:17:40 +0300

    Update 03_stream_of_letters.py

--- while_loops/while_loops_more/03_stream_of_letters.py
+++ while_loops/while_loops_more/03_stream_of_letters.py
@@ -15,7 +15,7 @@ while True:
     # we check if the the input letter is c, o, or n if it is and the
     # letter is not yet collected, we add it to the secret_sequence
     if ((input_letter == "c" or input_letter == "o" or input_letter == "n")
-            and input_letter not in secret_sequence):
+         and input_letter not in secret_sequence):
         secret_sequence += input_letter
         # if we have collected all 3 letters, we add the collected phrase
         # to the result and we reset the values of our holding variables
