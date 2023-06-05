0d8bd83ecacca098f62fbe65c77b8ca17937cc95
Петър Попов <124499412+pepcko@users.noreply.github.com>
2023-06-05 21:43:56 +0300

    Add files via upload

---
+++ nested_loops/nested_loops_more/11_happy_cat_parking.py
@@ -0,0 +1,24 @@
+number_days = int(input())
+number_hours = int(input())
+total_parking_tax = 0
+
+for day in range(1, number_days + 1):
+    daily_parking_tax = 0
+    for hour in range(1, number_hours + 1):
+
+        if day % 2 == 0 and hour % 2 != 0:
+            parking_tax = 2.5
+        
+        elif day % 2 != 0 and hour % 2 == 0:
+            parking_tax = 1.25
+        
+        else:
+            parking_tax = 1
+
+        daily_parking_tax += parking_tax
+    
+    total_parking_tax += daily_parking_tax
+    
+    print(f"Day: {day} - {daily_parking_tax:.2f} leva")
+
+print(f"Total: {total_parking_tax:.2f} leva")
